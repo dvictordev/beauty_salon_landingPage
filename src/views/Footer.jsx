12f2c8d788795530322e4f48bdb45932b169a647
victor <victor.d.dev@gmail.com>
2022-02-02 23:20:10 -0300

    added about page

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -1,26 +1,30 @@
 import Logo from '../assets/images/Logo.svg'
 import '../assets/styles/footer.css'
+import FaceLogo from '../assets/images/facebook.svg'
+import InstaLogo from '../assets/images/instagram.svg'
+import YoutubeLogo from '../assets/images/youtube.svg'
+import FooterLogo from '../assets/images/footerLogo.svg'
 
 export function Footer(props) {
   return (
     <footer>
-      <div className="container">
+      <div className="footer-container">
         <div className="footer-items">
           <div className="footer-desc">
-            <img src={Logo} alt="logo" />
+            <img src={FooterLogo} alt="logo" />
             <p>©2021 Beautysalon.</p>
             <p>Todos os direitos reservados.</p>
           </div>
           <div className="footer-social">
             <ul>
               <li>
-                <img src="" alt="" />
+                <img src={InstaLogo} alt=" logo instagram" />
               </li>
               <li>
-                <img src="" alt="" />
+                <img src={FaceLogo} alt="" />
               </li>
               <li>
-                <img src="" alt="" />
+                <img src={YoutubeLogo} alt="" />
               </li>
             </ul>
           </div>
